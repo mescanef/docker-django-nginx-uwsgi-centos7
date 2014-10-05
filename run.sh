aeed00fb33f818a67b8b07acdaa072335cb4b5d2
Mateusz Matuszkowiak <zone@mescanef.net>
2014-10-05 21:38:17 +0200

    chowned socket; set more restricted perms - 660

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # run uwsgi in background
 DOMAIN=`cat /.django` 
-su - djangouser -c "cd /${DOMAIN}/cfg/ && uwsgi --ini django.ini --uid 1000 --gid 1000 &"
+uwsgi --ini /${DOMAIN}/cfg/django.ini --uid 1000 --gid 1000 &
 
 # start nginx
 exec nginx
